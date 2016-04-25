ccf2878124ae1e57705ff5557e5cf93983776f77
Mitesh dave <Miteshdv@yahoo.co.in>
2016-04-25 12:51:35 +0530

    Test from eclipse

--- actions/GetGridData.js
+++ actions/GetGridData.js
@@ -1,6 +1,6 @@
 import fetch from 'isomorphic-fetch'
 
-export const REQUEST_POSTS = 'REQUEST_POSTS';
+export const REQUEST_POSTS = 'REQUEST_POSTS'
 export const RECEIVE_POSTS = 'RECEIVE_POSTS'
 
 function requestPosts() {
